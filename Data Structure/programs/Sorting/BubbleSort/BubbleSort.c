9ddd23b291cf41edb5e2d9ad67ce51228325e093
Badal Wanjari <91711831+badalwanjari@users.noreply.github.com>
2022-02-22 20:19:32 +0530

    Logic correction

--- Data Structure/programs/Sorting/BubbleSort/BubbleSort.c
+++ Data Structure/programs/Sorting/BubbleSort/BubbleSort.c
@@ -2,11 +2,11 @@
 
 void bubbleSort(int a[], int n){
 	for(int i=0;i<n;i++){
-		for(int j=i+1;j<n;j++){
-			if(a[i]>a[j]){
-				int temp=a[i];
-				a[i]=a[j];
-				a[j]=temp;
+		for(int j=0;j<n;j++){
+			if(a[j]>a[j+1]){
+				int temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
 			}
 		}
 	}
